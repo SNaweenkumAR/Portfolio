184671c8bc06f57d71f112b5f81b006603a352db
SNaweenkumAR <naween1924@gmail.com>
2024-11-25 15:08:37 +0530

    changes in import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
 import Services from './Components/Services/Services';
-import Mywork from './Components/Mywork/Mywork';
+import Mywork from './Components/Mywork/Mywork.jsx';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 
