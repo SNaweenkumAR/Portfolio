f64d8c57f947a4e3e2e18bb8b303493ab5c082ba
SNaweenkumAR <naween1924@gmail.com>
2024-12-05 11:57:59 +0530

    change the jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import About from './Components/About/About';
-import Services from './Components/Services/Services';
-import Mywork from './Components/Mywork/Mywork';
-import Contact from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Hero from './Components/Hero/Hero.jsx';
+import About from './Components/About/About.jsx';
+import Services from './Components/Services/Services.jsx';
+import Mywork from './Components/Mywork/Mywork.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
 
 
